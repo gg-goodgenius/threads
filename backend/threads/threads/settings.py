db23e2765025f8314b8b1a6a4455a06cdbe0db3e
ruha02 <ruslan.gzn@gmail.com>
2022-06-11 02:34:47 +0500

    Fixed docker-compose

--- backend/threads/threads/settings.py
+++ backend/threads/threads/settings.py
@@ -4,7 +4,7 @@ env = environ.Env()
 
 
 BASE_DIR = Path(__file__).resolve().parent.parent
-environ.Env.read_env(BASE_DIR.parent / '.env')
+environ.Env.read_env(BASE_DIR / '.env')
 
 print(BASE_DIR.parent)
 SECRET_KEY = env.str('SECRET_KEY', default='')
