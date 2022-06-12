15c63007d9e7c78ffa08d3f490732fa7454465d6
ruha02 <ruslan.gzn@gmail.com>
2022-06-12 12:58:42 +0500

    Fixed ursl.py

--- backend/threads/threads/urls.py
+++ backend/threads/threads/urls.py
@@ -4,10 +4,11 @@ from django.conf import settings
 
 from graphene_django.views import GraphQLView
 from graphql_jwt.decorators import jwt_cookie
+from django.views.decorators.csrf import csrf_exempt
 
 
 urlpatterns = [
-    path('', jwt_cookie(GraphQLView.as_view(graphiql=True))),
+    path('', jwt_cookie(csrf_exempt(GraphQLView.as_view(graphiql=True)))),
 ]
 
 if settings.ADMINSITE:
