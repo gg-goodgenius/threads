4558f4cf7ceb0aee97808ef2081095ead20c3075
semgoSE <semgoSE@gmail.com>
2022-06-13 03:38:53 +0300

    change page event

--- frontend/src/event/Event.component/Event.component.tsx
+++ frontend/src/event/Event.component/Event.component.tsx
@@ -1,7 +1,7 @@
-import {Card} from "../../Components/common/Card";
+import { Card } from "../../Components/common/Card";
 import './scss/index.scss';
-import {Tag} from "../../Components/common/Tag";
-import {ComingSoon} from "../../Components/common/ComingSoon/ComingSoon";
+import { Tag } from "../../Components/common/Tag";
+import { ComingSoon } from "../../Components/common/ComingSoon/ComingSoon";
 import { PrimaryButton } from "../../Components/common/PrimaryButtton";
 
 type Props = {
@@ -9,15 +9,17 @@ type Props = {
 }
 
 export const EventComponent = (props: Props) => {
-    return(
+    return (
         <div className='page-event'>
             <Card stretched disable>
                 <div className='card-event'>
                     <h1 className='title'>{props.event?.title}</h1>
-                    <img className='image' src={props.event?.image} />
-                    <span className='description'>
-                        dsfsaaaaaaaaaaaaaa
-                    </span>
+                    <div className='info'>
+                        <img className='image' src={props.event?.image}/>
+                        <span className='description'>
+                            {props.event?.descriptionOther}
+                        </span>
+                    </div>
                     <PrimaryButton>Записаться</PrimaryButton>
                 </div>
             </Card>
